1268a116c987577176b86df3200dd38eb29c1741
PioKl <kosiu342@gmail.com>
2025-01-31 20:58:43 +0100

    Add Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Header />
+      <main></main>
+      <Footer />
     </>
   );
 }
