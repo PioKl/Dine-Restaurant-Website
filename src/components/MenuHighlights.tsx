e085dd21113978f6395fbc5d442bb1d333c58b4f
PioKl <kosiu342@gmail.com>
2025-02-03 21:31:33 +0100

    Add MenuHighlights component

---
+++ src/components/MenuHighlights.tsx
@@ -0,0 +1,7 @@
+import styles from "../styles/menuHighlights.module.scss";
+
+export default function MenuHighlights() {
+  return (
+    <section className={`${styles["menu-section"]}`}>MenuHighlights</section>
+  );
+}
