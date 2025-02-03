18f07554eb98af38271a0e8cead9812a48eb35b1
PioKl <kosiu342@gmail.com>
2025-02-03 21:28:09 +0100

    Add Hero, RestaurantHighlights, MenuHighlights component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import Header from "./components/Header";
+import Hero from "./components/Hero";
+import RestaurantHighlights from "./components/RestaurantHighlights";
+import MenuHighlights from "./components/MenuHighlights";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <main></main>
+      <Hero />
+      <main>
+        <RestaurantHighlights
+          option={"first"}
+          heading={"Enjoyable place for all the family"}
+          description={
+            "Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal."
+          }
+        />
+
+        <RestaurantHighlights
+          option={"second"}
+          heading={"The most locally sourced food"}
+          description={
+            "All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food."
+          }
+        />
+        <MenuHighlights />
+        <RestaurantHighlights
+          option={"third"}
+          heading={"Family Gathering"}
+          description={
+            "We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all."
+          }
+          menuOptions={["Family Gathering", "Special Events", "Social Events"]}
+        />
+      </main>
       <Footer />
     </>
   );
