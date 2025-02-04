7365d5214e0f225f04eeee613d3e60e4488fc26f
PioKl <kosiu342@gmail.com>
2025-02-04 22:26:41 +0100

    Changes for option third i RestaurantHighlights

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,23 @@ function App() {
         <MenuHighlights />
         <RestaurantHighlights
           option={"third"}
-          heading={"Family Gathering"}
-          description={
-            "We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all."
-          }
-          menuOptions={["Family Gathering", "Special Events", "Social Events"]}
+          menuOptions={[
+            {
+              heading: "Family Gathering",
+              description:
+                "We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.",
+            },
+            {
+              heading: "Special Events",
+              description:
+                "Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.",
+            },
+            {
+              heading: "Social Events",
+              description:
+                "Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.",
+            },
+          ]}
         />
       </main>
       <Footer />
