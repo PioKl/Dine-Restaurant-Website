9c332341be631807b1307c94a9c4d2bade11701b
PioKl <kosiu342@gmail.com>
2025-02-05 19:28:13 +0100

    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,34 @@
+import styles from "../styles/footer.module.scss";
+import logo from "../assets/logo.svg";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={`wrapper ${styles["footer"]}`}>
+      <div className={`${styles["logo-container"]}`}>
+        <a
+          className={styles["logo-container__logo-link"]}
+          href="/"
+          aria-label="homepage"
+        >
+          <img
+            className={styles["logo-container__logo-image"]}
+            src={logo}
+            alt="Restaurant Logo"
+          />
+        </a>
+      </div>
+      <div className={`${styles["contact-details"]}`}>
+        <ul>
+          <li>Marthwaite, Sedbergh</li>
+          <li>Cumbria</li>
+          <li>+00 44 123 4567</li>
+        </ul>
+        <ul>
+          <li>Open Times</li>
+          <li>MON - FRI: 09:00 AM - 10:00 PM</li>
+          <li>SAT - SUN: 09:00 AM - 11:30 PM</li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
