2a9e4f13143c7e5f82132768de7c5a06d7e89710
PioKl <kosiu342@gmail.com>
2025-02-11 18:05:24 +0100

    More universal function for inputs

--- src/components/Reservations.tsx
+++ src/components/Reservations.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import Button from "./Button";
 import styles from "../styles/reservations.module.scss";
 import logo from "../assets/logo.svg";
 import patternCurveBottomRight from "../assets/patterns/pattern-curve-bottom-right.svg";
@@ -55,160 +56,144 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
     incompleteMessage: "The field is incomplete",
   };
 
-  const handleMonthInput = (value: string, isBlur: boolean = false) => {
+  const handleFormInput = (
+    value: string,
+    isBlur: boolean = false,
+    valueToUpdate: keyof typeof values
+  ) => {
     // Pozostawia pustą wartość, jeśli użytkownik usuwa wszystko
     if (value === "") {
-      setValues({ ...values, month: "" });
+      setValues((prevValues) => ({ ...prevValues, [valueToUpdate]: "" }));
       return;
     }
-
     // Usuwa znaki inne niż cyfry
     value = value.replace(/\D/g, "");
 
+    /*===================================
+                Dla Month
+    ======================================*/
     // Obsługa na `onBlur`
-    if (isBlur) {
-      if (value === "0" || value === "00" || value === "") {
-        value = "01"; // Minimalna wartość
-      } else if (value.length === 1) {
-        value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
-      }
-    }
-
-    // Blokada wpisywanych wartości spoza zakresu 01-12 (ale jest możliwość wpisania 0 i jego poprawienia, w celu utworzenia 01 itd)
-    if (!isBlur && value.length <= 2 && parseInt(value) > 12) {
-      return;
-    }
-
-    setValues({ ...values, month: value });
-  };
-
-  const handleDayInput = (value: string, isBlur: boolean = false) => {
-    // Pozostawia pustą wartość, jeśli użytkownik usuwa wszystko
-    if (value === "") {
-      setValues({ ...values, day: "" });
-      return;
-    }
-
-    // Usuwa znaki inne niż cyfry
-    value = value.replace(/\D/g, "");
-
-    // Obsługa na `onBlur`
-    if (isBlur) {
-      if (value === "0" || value === "00" || value === "") {
-        value = "01"; // Minimalna wartość
-      } else if (value.length === 1) {
-        value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
-      }
-    }
-
-    // Blokada wpisywanych wartości spoza zakresu 01-31 (ale jest możliwość wpisania 0 i jego poprawienia, w celu utworzenia 01 itd)
-    if (!isBlur && value.length <= 2 && parseInt(value) > 31) {
-      return;
-    }
-
-    setValues({ ...values, day: value });
-  };
-
-  const handleHourInput = (value: string, isBlur: boolean = false) => {
-    // Pozostawia pustą wartość, jeśli użytkownik usuwa wszystko
-    if (value === "") {
-      setValues({ ...values, hour: "" });
-      return;
-    }
-
-    // Usuwa znaki inne niż cyfry
-    value = value.replace(/\D/g, "");
-
-    // Obsługa na `onBlur`
-    if (isBlur) {
-      if (value === "0" || value === "00" || value === "") {
-        if (values.timeAbbreviation === "AM") {
-          value = "09"; // Minimalna wartość
-        } else {
-          value = "01"; // Jeśli inna od AM, czyli PM to minimalna wartość wynosi 01
+    if (valueToUpdate === "month") {
+      if (isBlur) {
+        if (value === "0" || value === "00" || value === "") {
+          value = "01"; // Minimalna wartość
+        } else if (value.length === 1) {
+          value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
         }
-      } else if (value.length === 1 && value === "1") {
-        value = value + "0"; // Dodaje zero, np. "1" to utworzy "10"
-      } else if (value.length === 1) {
-        value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
       }
-    }
 
-    // Blokada wpisywanych wartości oprócz 1, 0 i zakresu od 9 do 11 dla AM oraz blokada wszystkich wartości, oprócz 1, 0 i zakresu od 1 do 12 dla PM
-    if (!isBlur && value.length <= 2) {
-      if (
-        values.timeAbbreviation === "AM" &&
-        (parseInt(value) === 1 ||
-          parseInt(value) === 0 ||
-          (parseInt(value) >= 9 && parseInt(value) <= 11))
-      ) {
-        setValues({ ...values, hour: value });
-      } else if (
-        values.timeAbbreviation === "PM" &&
-        (parseInt(value) === 1 ||
-          parseInt(value) === 0 ||
-          (parseInt(value) >= 1 && parseInt(value) <= 12))
-      ) {
-        setValues({ ...values, hour: value });
+      // Blokada wpisywanych wartości spoza zakresu 01-12 (ale jest możliwość wpisania 0 i jego poprawienia, w celu utworzenia 01 itd)
+      if (!isBlur && value.length <= 2 && parseInt(value) > 12) {
+        return;
       }
-      return;
-    }
-
-    setValues({ ...values, hour: value });
-  };
-
-  const handleMinutesInput = (value: string, isBlur: boolean = false) => {
-    // Pozostawia pustą wartość, jeśli użytkownik usuwa wszystko
-    if (value === "") {
-      setValues({ ...values, minutes: "" });
-      return;
-    }
-
-    // Usuwa znaki inne niż cyfry
-    value = value.replace(/\D/g, "");
-
-    // Obsługa na `onBlur`
-    if (isBlur) {
-      if (value === "0" || value === "") {
-        value = "00"; // Minimalna wartość
-      } else if (value.length === 1) {
-        value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
+      //month: value -> tak to wygląda
+      setValues((prevValues) => ({ ...prevValues, [valueToUpdate]: value }));
+    } else if (valueToUpdate === "day") {
+      /* ===================================
+                Dla Day
+    ======================================*/
+      // Obsługa na `onBlur`
+      if (isBlur) {
+        if (value === "0" || value === "00" || value === "") {
+          value = "01"; // Minimalna wartość
+        } else if (value.length === 1) {
+          value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
+        }
       }
-    }
 
-    // Blokada wpisywanych wartości spoza zakresu 00-59
-    if (!isBlur && value.length <= 2 && parseInt(value) > 59) {
-      return;
-    }
-
-    setValues({ ...values, minutes: value });
-  };
-
-  const handleYearInput = (value: string, isBlur: boolean = false) => {
-    // Pozostawia pustą wartość, jeśli użytkownik usuwa wszystko
-    if (value === "") {
-      setValues({ ...values, year: "" });
-      return;
-    }
-    // Usuwa znaki inne niż cyfry
-    value = value.replace(/\D/g, "");
-
-    // Obsługa na `onBlur`
-    if (isBlur) {
-      if (value !== "2025" && value !== "2026" && value !== "") {
-        //Jeśli wartości są różne niż 2025 i 2026 wtedu ustawi 2025, aktualny rok
-        value = "2025";
-      } else if (value.length <= 3) {
-        //także w przypadku, gdy długość wpisywanego znaku jest <=3
-        value = "2025";
+      // Blokada wpisywanych wartości spoza zakresu 01-31 (ale jest możliwość wpisania 0 i jego poprawienia, w celu utworzenia 01 itd)
+      if (!isBlur && value.length <= 2 && parseInt(value) > 31) {
+        return;
       }
-    }
 
-    // Blokada wpisywanych wartości większych niż 2026
-    if (!isBlur && parseInt(value) > 2026) {
-      return;
+      //day: value -> tak to wygląda
+      setValues((prevValues) => ({ ...prevValues, [valueToUpdate]: value }));
+    } else if (valueToUpdate === "year") {
+      /* ===================================
+                Dla Year
+       ======================================*/
+      // Obsługa na `onBlur`
+      if (isBlur) {
+        if (value !== "2025" && value !== "2026" && value !== "") {
+          //Jeśli wartości są różne niż 2025 i 2026 wtedy ustawi 2025, aktualny rok
+          value = "2025";
+        } else if (value.length <= 3) {
+          //także w przypadku, gdy długość wpisywanego znaku jest <=3
+          value = "2025";
+        }
+      }
+
+      // Blokada wpisywanych wartości większych niż 2026
+      if (!isBlur && parseInt(value) > 2026) {
+        return;
+      }
+      setValues((prevValues) => ({ ...prevValues, [valueToUpdate]: value }));
+    } else if (valueToUpdate === "hour") {
+      /* ===================================
+                Dla Hour
+       ======================================*/
+      // Obsługa na `onBlur`
+      if (isBlur) {
+        if (value === "0" || value === "00" || value === "") {
+          if (values.timeAbbreviation === "AM") {
+            value = "09"; // Minimalna wartość
+          } else {
+            value = "01"; // Jeśli inna od AM, czyli PM to minimalna wartość wynosi 01
+          }
+        } else if (value.length === 1 && value === "1") {
+          value = value + "0"; // Dodaje zero, np. "1" to utworzy "10"
+        } else if (value.length === 1) {
+          value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
+        }
+      }
+
+      // Blokada wpisywanych wartości oprócz 1, 0 i zakresu od 9 do 11 dla AM oraz blokada wszystkich wartości, oprócz 1, 0 i zakresu od 1 do 12 dla PM
+      if (!isBlur && value.length <= 2) {
+        if (
+          values.timeAbbreviation === "AM" &&
+          (parseInt(value) === 1 ||
+            parseInt(value) === 0 ||
+            (parseInt(value) >= 9 && parseInt(value) <= 11))
+        ) {
+          setValues((prevVaules) => ({
+            ...prevVaules,
+            [valueToUpdate]: value,
+          }));
+        } else if (
+          values.timeAbbreviation === "PM" &&
+          (parseInt(value) === 1 ||
+            parseInt(value) === 0 ||
+            (parseInt(value) >= 1 && parseInt(value) <= 12))
+        ) {
+          setValues((prevVaules) => ({
+            ...prevVaules,
+            [valueToUpdate]: value,
+          }));
+        }
+        return;
+      }
+
+      setValues((prevVaules) => ({ ...prevVaules, [valueToUpdate]: value }));
+    } else if (valueToUpdate === "minutes") {
+      /* ===================================
+                Dla Minutes
+       ======================================*/
+      // Obsługa na `onBlur`
+      if (isBlur) {
+        if (value === "0" || value === "") {
+          value = "00"; // Minimalna wartość
+        } else if (value.length === 1) {
+          value = "0" + value; // Dodaje zero, np. "1" to utworzy "01"
+        }
+      }
+
+      // Blokada wpisywanych wartości spoza zakresu 00-59
+      if (!isBlur && value.length <= 2 && parseInt(value) > 59) {
+        return;
+      }
+
+      setValues((prevValues) => ({ ...prevValues, [valueToUpdate]: value }));
     }
-    setValues({ ...values, year: value });
   };
 
   //Jeśli użytkownik naciśnie enter będąc na jakimś inpucie niech odpali jeszcze funkcję z onChange, jest to w razie tego, że niekiedy jak użytkownik opuści focus wtedy zmienia się wartość na prawidłową, jeśli była nieprawidłowa, natomiast jeżeli miał wartość nieprawidłową i wcisnął enter wtedy go przepuszczało, ta funkcja zachowa się tak jakby opuścił focus, dzięki temu przejdzie tylko wartość prawidłowa
@@ -304,22 +289,35 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
             {type === "banner" && <h2>Ready to make a reservation?</h2>}
             {type === "hero" && (
               <>
-                <img
-                  className={`${styles["reservations-content__logo-image"]}`}
-                  src={logo}
-                  alt="Restaurant Logo"
-                />
+                <a href="/">
+                  <img
+                    className={`${styles["reservations-content__logo-image"]}`}
+                    src={logo}
+                    alt="Restaurant Logo"
+                  />
+                </a>
+
                 <h1>Reservations</h1>
                 <p>
                   We can’t wait to host you. If you have any special
                   requirements please feel free to call on the phone number
                   below. We’ll be happy to accommodate you.
                 </p>
-                <button className={`btn`}>Reserve Place</button>
+                <a href="#form">
+                  <Button
+                    buttonType="normal"
+                    linkToBooking={false}
+                    text="Reserve Place"
+                  />
+                </a>
               </>
             )}
             {type === "banner" && (
-              <button className={`btn`}>Book a Table</button>
+              <Button
+                buttonType="normal"
+                linkToBooking={true}
+                text="Book a Table"
+              />
             )}
           </div>
         </div>
@@ -331,17 +329,16 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                 src={patternCurveBottomRight}
                 alt="pattern-curve"
               />
-              <div className={`${styles["reservation__form-container"]}`}>
+              <div
+                id="form"
+                className={`${styles["reservation__form-container"]}`}
+              >
                 <img
                   className={`${styles["reservation__pattern-lines"]}`}
                   src={patternLines}
                   alt="patterin-lines"
                 />
-                <form
-                  onSubmit={handleSubmit}
-                  className={` ${styles["form"]}`}
-                  action=""
-                >
+                <form onSubmit={handleSubmit} className={` ${styles["form"]}`}>
                   <div className={`${styles["form__guest-info-container"]}`}>
                     <label htmlFor="name"></label>
                     <input
@@ -413,11 +410,15 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                           id="month"
                           maxLength={2}
                           value={values.month}
-                          onChange={(e) => handleMonthInput(e.target.value)}
-                          onBlur={(e) => handleMonthInput(e.target.value, true)}
+                          onChange={(e) =>
+                            handleFormInput(e.target.value, false, "month")
+                          }
+                          onBlur={(e) =>
+                            handleFormInput(e.target.value, true, "month")
+                          }
                           onKeyDown={(e) =>
                             handleKeyDown(e, () =>
-                              handleMonthInput(values.month, true)
+                              handleFormInput(values.month, true, "month")
                             )
                           }
                         />
@@ -432,11 +433,15 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                           id="day"
                           maxLength={2}
                           value={values.day}
-                          onChange={(e) => handleDayInput(e.target.value)}
-                          onBlur={(e) => handleDayInput(e.target.value, true)}
+                          onChange={(e) =>
+                            handleFormInput(e.target.value, false, "day")
+                          }
+                          onBlur={(e) =>
+                            handleFormInput(e.target.value, true, "day")
+                          }
                           onKeyDown={(e) =>
                             handleKeyDown(e, () =>
-                              handleDayInput(values.day, true)
+                              handleFormInput(values.month, true, "day")
                             )
                           }
                         />
@@ -451,11 +456,15 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                           id="year"
                           maxLength={4}
                           value={values.year}
-                          onChange={(e) => handleYearInput(e.target.value)}
-                          onBlur={(e) => handleYearInput(e.target.value, true)}
+                          onChange={(e) =>
+                            handleFormInput(e.target.value, false, "year")
+                          }
+                          onBlur={(e) =>
+                            handleFormInput(e.target.value, true, "year")
+                          }
                           onKeyDown={(e) =>
                             handleKeyDown(e, () =>
-                              handleYearInput(values.year, true)
+                              handleFormInput(values.month, true, "year")
                             )
                           }
                         />
@@ -495,11 +504,15 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                           id="hour"
                           maxLength={2}
                           value={values.hour}
-                          onChange={(e) => handleHourInput(e.target.value)}
-                          onBlur={(e) => handleHourInput(e.target.value, true)}
+                          onChange={(e) =>
+                            handleFormInput(e.target.value, false, "hour")
+                          }
+                          onBlur={(e) =>
+                            handleFormInput(e.target.value, true, "hour")
+                          }
                           onKeyDown={(e) =>
                             handleKeyDown(e, () =>
-                              handleHourInput(values.hour, true)
+                              handleFormInput(values.month, true, "hour")
                             )
                           }
                         />
@@ -514,13 +527,15 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                           id="minutes"
                           maxLength={2}
                           value={values.minutes}
-                          onChange={(e) => handleMinutesInput(e.target.value)}
+                          onChange={(e) =>
+                            handleFormInput(e.target.value, false, "minutes")
+                          }
                           onBlur={(e) =>
-                            handleMinutesInput(e.target.value, true)
+                            handleFormInput(e.target.value, true, "minutes")
                           }
                           onKeyDown={(e) =>
                             handleKeyDown(e, () =>
-                              handleMinutesInput(values.minutes, true)
+                              handleFormInput(values.month, true, "minutes")
                             )
                           }
                         />
@@ -586,12 +601,12 @@ const Reservations: React.FC<ReservationsProps> = ({ type }) => {
                       +
                     </button>
                   </div>
-                  <button
-                    type="submit"
-                    className={`btn --dark ${styles["form__button"]}`}
-                  >
-                    Make Reservation
-                  </button>
+                  <Button
+                    buttonType="dark"
+                    linkToBooking={false}
+                    text="Make Reservation"
+                    className={styles["form__button"]}
+                  />
                 </form>
               </div>
             </div>
