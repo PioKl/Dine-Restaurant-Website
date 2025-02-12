563a560559313c406137edf63cd6a13e6d076b83
PioKl <kosiu342@gmail.com>
2025-02-12 18:41:35 +0100

    Add framer motion animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import logo from "../assets/logo.svg";
 import styles from "../styles/hero.module.scss";
 import Button from "./Button";
@@ -12,13 +13,29 @@ export default function Hero() {
             <Link className={styles["hero__logo"]} to="/" aria-label="homepage">
               <img src={logo} alt="Restaurant Logo" />
             </Link>
-            <h1 className={styles["hero__heading"]}>
+            <motion.h1
+              className={styles["hero__heading"]}
+              viewport={{ once: true }}
+              initial={{ x: "-25%" }}
+              whileInView={{
+                x: "0",
+                transition: { duration: 0.75, ease: "easeInOut" },
+              }}
+            >
               Exquisite dining since 1989
-            </h1>
-            <p className={styles["hero__info"]}>
+            </motion.h1>
+            <motion.p
+              className={styles["hero__info"]}
+              viewport={{ once: true }}
+              initial={{ x: "25%" }}
+              whileInView={{
+                x: "0",
+                transition: { duration: 0.75, ease: "easeInOut" },
+              }}
+            >
               Experience our seasonal menu in beautiful country surroundings.
               Eat the freshest produce from the comfort of our farmhouse.
-            </p>
+            </motion.p>
             <Button
               buttonType="normal"
               linkToBooking={true}
